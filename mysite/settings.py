1efee2166823f272b8d0e9488493faec7b851bb2
mark0503 <m.perschin0503@gmail.com>
2021-06-01 19:28:55 +0300

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url 
+
+db_from_env = dj_database_url.config()
+
+\
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
